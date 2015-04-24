0714d0bee13e4f38199c19d8091f3b9149ff859d
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2015-04-24 16:58:13 +0900

    add stuff

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php phpinfo(); ?>
+<html>
+<head>
+	<title>Mobingi PHP default app</title>
+</head>
+<body>
+	<h1>Mobingi PHP default app</h1>
+	<p>Welcome to Mobingi! This is a test application. Here are some pages for you:
+	<ul>
+		<li><a href="phpinfo.php">PHP Info</a></li>
+		<li><a href="testgd.php">A little random image generator</a></li>
+	</ul>
+</body>
+</html>
