d59d7f21d70b03f297242c84db3e7321cf1febdc
HARUKA <horioka.h@gmail.com>
2017-02-07 12:23:19 +0900

    change to new version

--- index2.php
+++ index2.php
@@ -10,11 +10,11 @@
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-
         <!-- Custom CSS -->
         <link href="css/styles.css" rel="stylesheet">
+
+        <!-- Font Awesome -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,7 +38,7 @@
                     <div class="col-sm-6 content1">
 
                         <h1>おめでとうございます！</h1>
-                        <p>moCloudアプリケーションは現在あなた専用の環境で動作しています。</p>
+                        <p>あなたのMobingi PHP アプリケーションは現在あなた専用の環境で動作しています。</p>
                         <p><?php echo ' PHP version: ' . phpversion(); ?> で動いています。</p>
 
                     </div>
@@ -48,25 +48,25 @@
                     <div class="col-sm-6 content2">
                         <h2><i class="fa fa-files-o" aria-hidden="true"></i>What's Next?</h2>
                         <ul>
-                            <li><a href="http://mocloud.io/documentation_jp/overview/features" target="_blank">moCloud ドキュメント</a></li>
-                            <li><a href="http://docs.mobingi.com/jp/overview/features" target="_blank">Mobingi For Enterprise ドキュメント</a></li>
+                            <li><a href="https://docs.mobingi.com/official/getting-started/jp" target="_blank">Mobingi ドキュメント</a></li>
                         </ul>
 
                         <h2><i class="fa fa-cogs" aria-hidden="true"></i>チュートリアル</h2>
                         <ul>
-                            <li><a href="http://yoshidashingo.hatenablog.com/entry/2016/01/06/200919" target="_blank">moCloudでProcfileを使ってNode.jsアプリを動かす </a></li>
+                            <li><a href="https://docs.mobingi.com/official/tutorials/jp/ssh_into_your_server" target="_blank">サーバーにSSHでログインする</a></li>
+                            <li><a href="https://docs.mobingi.com/official/tutorials/jp/custom_installation_script" target="_blank">カスタムスクリプトを記述する<small>(mobingi-install.sh)</small></a></li>
+                            <li><a href="http://yoshidashingo.hatenablog.com/entry/2016/01/06/200919" target="_blank">moCloudでProcfileを使ってNode.jsアプリを動かす</a></li>
                             <li><a href="https://medium.com/@waylandzhang/deploy-your-first-php-application-on-mobingi-in-5-minutes-9207ad417b21#.a6jtlo7gh" target="_blank">Deploy your first PHP application on Mobingi Cloud in 5 minutes [英語]</a></li>
                         </ul>
 
                         <h2><i class="fa fa-life-ring" aria-hidden="true"></i>カスタマーサービス</h2>
                         <ul>
-                            <li><a href="https://mocloud.io/documentation_jp/support/email" target="_blank">サポートメール</a></li>
+                            <li><a href="https://docs.mobingi.com/official/support/jp" target="_blank">サポートページ</a></li>
                         </ul>
 
                         <h2><i class="fa fa-external-link" aria-hidden="true"></i>リンク</h2>
                         <ul>
                             <li><a href="https://mobingi.co.jp/" target="_blank">mobingi.com</a></li>
-                            <li><a href="https://mocloud.io/jp/" target="_blank">moCloud.io</a></li>
                         </ul>
 
                     </div>
